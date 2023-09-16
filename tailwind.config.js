42d9da3d3223c91c7767a7488c6f963630875779
Neway Tadesse <tad.neway@gmail.com>
2023-09-16 14:10:54 +0300

    first and final commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
+    },
+  },
+  plugins: [],
+};
